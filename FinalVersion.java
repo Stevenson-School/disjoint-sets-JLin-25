25013a41033609d91ab0b9e916e9da98bddbfb33
JLin-25 <151233145+JLin-25@users.noreply.github.com>
2023-12-07 11:22:07 -0800

    Create FinalVersion.java

---
+++ FinalVersion.java
@@ -0,0 +1,25 @@
+public class FinalVersion implements DisjointSet{
+    int[] fa;
+    public FinalVersion(int size){
+        fa=new int[size];
+        for(int i=0;i<size;i++)fa[i]=i;
+    }
+    public void connect(int x,int y){
+        fa[find(x)]=find(y);
+    }
+    public boolean isConnected(int x,int y){
+        return find(x)==find(y);
+    }
+    private int find(int x){
+        return fa[x]=fa[x]==x?x:find(fa[x]);
+    }
+    public static void main(String[] args) {
+        FinalVersion x=new FinalVersion(8);
+        x.connect(3,2);
+        x.connect(5,3);
+        x.connect(6,7);
+        x.connect(7,1);
+        x.connect(1,2);
+        System.out.println(x.isConnected(5,7));
+    }
+}
